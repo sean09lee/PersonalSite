91cb88b6c9b6dc511ce3aea17408f43bea22ccb5
Sean Lee <sean09lee@gmail.com>
2015-08-23 02:07:29 -0500

    advanced parallax scrolling

---
+++ app/js/parallax.js
@@ -0,0 +1,16 @@
+(function(){
+
+  var parallax = document.querySelectorAll(".parallax"),
+      speed = 0.5;
+
+  window.onscroll = function(){
+    [].slice.call(parallax).forEach(function(el,i){
+
+        var windowYOffset = window.pageYOffset,
+            elementYOffset = el.offsetTop, elBackgrounPos = "50% " + ((windowYOffset - elementYOffset) * speed) + "px";
+      el.style.backgroundPosition = elBackgrounPos;
+
+    });
+  };
+
+})();
