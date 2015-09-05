c46775f99db57b3148f2c98031c08fe48b97b2b5
Sean Lee <sean09lee@gmail.com>
2015-09-05 18:24:56 -0500

    Background image tint and new pages

--- app/js/app.js
+++ app/js/app.js
@@ -2,24 +2,33 @@
 
 /* App Module */
 
-var personalApp = angular.module('personalApp', [
-  'ngRoute',
-  'personalControllers',
-  'personalServices'
-]);
+var personalApp = angular.module('personalApp', ['ngRoute']);
 
-personalApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/phones', {
-        templateUrl: 'partials/phone-list.html',
-        controller: 'PhoneListCtrl'
-      }).
-      when('/phones/:phoneId', {
-        templateUrl: 'partials/phone-detail.html',
-        controller: 'PhoneDetailCtrl'
-      }).
-      otherwise({
-        redirectTo: '/phones'
-      });
-  }]);
+personalApp.controller('mainController', function($scope) {
+    // create a message to display in our view
+    $scope.message = 'Everyone come and see how good I look!';
+});
+
+
+// configure our routes
+personalApp.config(function($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl : 'home.html',
+            controller  : 'mainController'
+        })
+
+        // route for the about page
+        .when('/about', {
+            templateUrl : 'pages/about.html',
+            controller  : 'aboutController'
+        })
+
+        // route for the contact page
+        .when('/contact', {
+            templateUrl : 'pages/contact.html',
+            controller  : 'contactController'
+        });
+});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,19 +4,16 @@
 
 var personalControllers = angular.module('personalControllers', []);
 
-personalControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
-  function($scope, Phone) {
-    $scope.phones = Phone.query();
-    $scope.orderProp = 'age';
-  }]);
+// create the controller and inject Angular's $scope
+personalControllers.controller('mainController', function($scope) {
+    // create a message to display in our view
+    $scope.message = 'Everyone come and see how good I look!';
+});
 
-personalControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
-  function($scope, $routeParams, Phone) {
-    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-      $scope.mainImageUrl = phone.images[0];
-    });
+personalControllers.controller('aboutController', function($scope) {
+    $scope.message = 'Look! I am an about page.';
+});
 
-    $scope.setImage = function(imageUrl) {
-      $scope.mainImageUrl = imageUrl;
-    };
-  }]);
+personalControllers.controller('contactController', function($scope) {
+    $scope.message = 'Contact us! JK. This is just a demo.';
+});
